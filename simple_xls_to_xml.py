00fb07c2c56ac6ea862c9eaa24013bb9721d9879
Tynox <pocktynox@gmail.com>
2014-01-15 21:29:03 +0800

    use re to remove whitespace

--- simple_xls_to_xml.py
+++ simple_xls_to_xml.py
@@ -7,14 +7,18 @@ import xml.dom.minidom
 
 filter_words = None
 
+
 def xlsRead():
+    # reade xls
     global filter_words
     
     data = xlrd.open_workbook("filter.xlsx")
     table = data.sheets()[0]        # 获取第一个sheet
     filter_words = table.col_values(0)
     
+    
 def createXML():
+    # create xml
     if filter_words is None:
         return
     
@@ -25,14 +29,23 @@ def createXML():
     for f in filter_words:
         if len(f) == 0:
             continue
+        # remove whitespace
+        whitespace = re.compile("\s")
+        f = re.sub(whitespace, "", f)
+        # create element
         filter = dom.createElement("filter")
-        f = f.strip()
         filter.setAttribute("word", f)
         root.appendChild(filter)
         
     out = codecs.open("filters.xml", "w", "utf-8")
     dom.writexml(out, addindent=" ", newl="\n", encoding="utf-8")
     out.close()
+    
+    
+def createCVS():
+    # TODO:创建CVS
+    pass
+    
     
 if __name__ == "__main__":
     xlsRead()
